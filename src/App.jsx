26e0bf0cdaab80d426e507d90d161fedea8a946c
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-05-17 13:28:10 +0300

    cleaned up some unused code

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,12 @@ import { Canvas, useThree } from '@react-three/fiber';
 import Fighter from './Components/Fighter';
 import './App.css';
 import { Suspense, useContext, useEffect, useRef, useState } from 'react';
-import { CameraHelper } from 'three';
+
 import { Map } from './Components/map';
 import { Physics } from '@react-three/cannon';
-import Laser from './Components/Weapons/Laser';
-import Gun from './Components/Weapons/Gun';
-import Boss from './Components/Boss';
-import Boss_Blob from './Components/Weapons/MagmaBall';
-import Boss2 from './Components/Boss2';
+
 import KrakenEye from './Components/KrakenEye';
-import Beam from './Components/Weapons/Beam';
-import GameOverText from './Components/GameOverText';
+
 import {
   EffectComposer,
   DepthOfField,
